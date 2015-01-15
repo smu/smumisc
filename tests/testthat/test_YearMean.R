703baff11c232e16863d2dc1811671fcac95b246
Stefan Muthers <muthers@climate.unibe.ch>
2015-01-15 12:09:29 +0100

    test against cdo output (still waiting for netcdf functions)

--- tests/testthat/test_YearMean.R
+++ tests/testthat/test_YearMean.R
@@ -11,4 +11,17 @@ test_that('Annual mean of annual data equals annual data.', {
         expect_that(all(mydata$value == YearMean(mydata)$value), is_true())
           })
 
-# TODO compare results against CDO('-seasmean')
+
+# compare results against CDO('-seasmean')
+fname = system.file("extdata", "example_tll.nc", package = "smumisc")
+test_that('Example netcdf file exists', {
+          expect_that(fname != '', is_true())
+})
+
+tmpfile <- tempfile()
+CDO('-yearmean -fldmean ',fname,' ',tmpfile)
+
+# TODO: load file when GetVarNCDFTLL are available
+
+
+unlink(tmpfile)
