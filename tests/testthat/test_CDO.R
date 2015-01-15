ad8e24b94cbd03af5bb48dab87fe08f6d7267080
Stefan Muthers <muthers@climate.unibe.ch>
2015-01-15 10:54:09 +0100

    + example netcdf file + test CDO function with example file.

--- tests/testthat/test_CDO.R
+++ tests/testthat/test_CDO.R
@@ -1,6 +1,28 @@
-
 library(smumisc)
 context('CDO')
 
 
-# TODO: will need some test data files
+fname = system.file("extdata", "example_tll.nc", package = "smumisc")
+test_that('Example netcdf file exists', {
+          expect_that(fname != '', is_true())
+})
+
+test_that('cdo is available', {
+          expect_that(SystemCmd('which cdo') == 0, is_true())
+          expect_that(SystemCmd('which cdo', intern=T) != '', is_true())
+})
+
+tmpfile <- tempfile()
+test_that('tmpfile should not be present', {
+          expect_that(file.exists(tmpfile), is_false())
+})
+
+test_that('CDO command is executed', {
+          expect_equal(CDO('-fldmean -yearmean ',fname,' ',tmpfile), 0)
+})
+
+test_that('CDO commands output exists', {
+          expect_that(file.exists(tmpfile), is_true())
+})
+
+unlink(tmpfile)
