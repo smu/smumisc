b096475d75e828ea3fdbd7bbc56e7c1d811c4ae1
Stefan Muthers <muthers@climate.unibe.ch>
2015-03-05 11:44:57 +0100

    new function: GlobalMean

---
+++ R/GlobalMean.R
@@ -0,0 +1,15 @@
+#' Calculate weighted global mean from data.table. Weighting is done by latitudes.
+#'  TODO: consider lon values
+#'
+#' @param d data table containing the column lat (latitude) and value (the variable)
+#' @return vector with global mean values.
+
+
+GlobalMean <- function(d){
+    lats <- unique(d$lat)          
+    rlats   = lats*pi/180         
+    colats <- cos(rlats)     
+    weights <- colats / sum(colats)   
+    gmean  <- sum(d$value * weights)
+    return(gmean)               
+}     
---
+++ tests/testthat/test_GlobalMean.R
@@ -0,0 +1,39 @@
+
+library(smumisc)
+library(plyr)
+context('GlobalMean')
+
+
+# compare results against CDO('-fldmean')
+fname = system.file("extdata", "example_tll.nc", package = "smumisc")
+test_that('Example netcdf file exists', {
+          expect_that(fname != '', is_true())
+})
+
+
+# since longitudes are not considered in GlobalMean (so far), we need to calculate the zonal mean first
+tmpfile1 <- tempfile(pattern='1')
+CDO('-zonmean ',fname,' ',tmpfile1)
+
+# cdo result
+tmpfile2 <- tempfile(pattern='2')
+CDO('-fldmean ',tmpfile1,' ',tmpfile2)
+d_cdo <- GetVarNCDF(tmpfile2, 'slp', 'time')
+
+
+# R result
+d <- GetVarNCDFTL(tmpfile1, 'slp')
+#  d_ <- na.omit(YearMean(d))
+d_R <- ddply(d, .(date), GlobalMean)
+
+
+
+test_that('YearMean == "cdo -fldmean"', {
+          expect_equal(d_R$V1[1], d_cdo$value[1], tolerance = 1e-6)
+          expect_equal(d_R$V1[nrow(d_R)], d_cdo$value[nrow(d_cdo)], tolerance = 1e-6)
+})
+
+
+
+unlink(tmpfile1)
+unlink(tmpfile2)
