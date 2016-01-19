fc9fc979502aaa1e20af82a2e5b3f4dc52d0f5dd
Stefan Muthers <muthers@climate.unibe.ch>
2016-01-19 17:44:20 +0100

    Documentation update for CDO()

--- R/CDO.R
+++ R/CDO.R
@@ -2,6 +2,8 @@
 #'
 #' @param ...  one or more arguments of type string, which can be concatenated
 #'             using the paste function (e.g. '-yseasmean', '-fldmean').
+#'             Additionally, parameter for the system function can be passed, e.g.,
+#'             'intern = TRUE' to capture the output of the cdo command.
 #' @param sep character used to seperate the terms. Used in the paste function.
 #' @param debug print cdo command before execution and call cdo without '-s' argument.
 #' @examples
