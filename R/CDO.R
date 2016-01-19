9c40e2168f37da3ce1958e592259878c88c446c1
Stefan Muthers <muthers@climate.unibe.ch>
2016-01-19 18:08:05 +0100

    a few more details in the CDO documentation.

--- R/CDO.R
+++ R/CDO.R
@@ -3,7 +3,7 @@
 #' @param ...  one or more arguments of type string, which can be concatenated
 #'             using the paste function (e.g. '-yseasmean', '-fldmean').
 #'             Additionally, parameter for the system function can be passed, e.g.,
-#'             'intern = TRUE' to capture the output of the cdo command.
+#'             \code{intern = TRUE} to capture the output of the cdo command.
 #' @param sep character used to seperate the terms. Used in the paste function.
 #' @param debug print cdo command before execution and call cdo without '-s' argument.
 #' @examples
@@ -11,7 +11,10 @@
 #'      tmpfile <- tempfile()
 #'      CDO('-fldmean -selyear,1980/1990',infile,tmpfile,sep=' ')
 #'
-#' 
+#' @section Details:
+#'      The behaviour of some cdo commands is control by environment variables.
+#'      These need to defined prior to the CDO call using \code{Sys.setenv(...)}, e.g,
+#'      \code{Sys.setenv('CDO_WEIGHT_MODE' = 'off')}
 #' 
 
 CDO <- function(..., sep = '', debug = FALSE){
