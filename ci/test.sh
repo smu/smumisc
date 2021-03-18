8c014f168ad4058bbb4613a76658f6cf90786d29
Stefan Muthers <smu@x201.local>
2021-03-18 20:43:11 +0100

    more dependencies and cdo

--- ci/test.sh
+++ ci/test.sh
@@ -4,9 +4,21 @@
 set -e 
 
 sudo R --vanilla <<EOF
-install.packages(c('RCurl','ncdf','testthat'), repos='http://cran.us.r-project.org')
+install.packages(c('RCurl', 
+                   'stringr',
+                   'ncdf4',
+                   'testthat'), repos='http://cran.us.r-project.org')
 q()
 EOF
+
+# install cdo
+
+wget https://code.mpimet.mpg.de/attachments/download/24638/cdo-1.9.10.tar.gz
+tar xvzf cdo-1.9.10.tar.gz
+cd cdo-1.9.10
+./configure 
+make
+sudo make install
 
 R CMD build .
 R CMD check *.tar.gz
